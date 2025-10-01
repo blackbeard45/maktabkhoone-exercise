e11f5c99c3131e7ec78e2d8e8fe5c66e9d412289
blackbeard45 <vilhoerno@gmail.com>
2025-10-01 12:12:26 +0330

    add basic teamplates to render.

--- website/urls.py
+++ website/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index', index_view),
+    path('', index_view),
     path('about', about_view),
     path('contact', contact_view),
 ]
