7398467712c4e727d247b719889741674c663f4d
blackbeard45 <vilhoerno@gmail.com>
2025-10-15 08:46:20 +0330

    create single pages and use user table as foreign key.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
 
+def test(request, name):
+    # posts = Post.objects.all()
+    context = {'name': name}
+    return render(request, 'test.html', context)
